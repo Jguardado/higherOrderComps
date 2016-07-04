e3c18f8a444bb1369c2683b03551cd3a99b91d88
Juan <jguardado85@gmail.com>
2016-07-04 10:19:54 -0700

    completed higher order component

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class Header extends Component {
+
   authButton() {
-    return <button>Sign In</button>;
+    if (this.props.authenticated) {
+      return <button onClick={() => this.props.authenticate(false)}>Sign Out</button>;
+    }
+
+    return <button onClick={() =>  this.props.authenticate(true)}>Sign In</button>;
   }
 
   render() {
@@ -11,10 +18,10 @@ class Header extends Component {
       <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
           <li className="nav-item">
-            <Link to="/">Home</Link>
+            <Link to='/'>Home</Link>
           </li>
           <li className="nav-item">
-            <Link to="/resources">Resources</Link>
+            <Link to='/resources'>Resources</Link>
           </li>
           <li className="nav-item">
             {this.authButton()}
@@ -25,4 +32,8 @@ class Header extends Component {
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return { authenticated: state.authenticated };
+}
+
+export default connect(mapStateToProps, actions)(Header);
---
+++ src/components/resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super Special Recipe
+      <ul>
+        <li> 1 cup Specual </li>
+        <li> 1 cup super </li>
+      </ul>
+    </div>
+  );
+};
